a53b2d8361effbabab7375caec98c82d494b184b
Dhiren Patel <dhirenspatel@hotmail.com>
2017-07-24 21:26:37 +0530

    Add project files.

---
+++ ELMAHErrorLoggin/App_Start/FilterConfig.cs
@@ -0,0 +1,16 @@
+﻿using ELMAHErrorLoggin.Models;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ELMAHErrorLoggin
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            //Below 1,2 is order to execute
+            filters.Add(new ElmahHandleErrorAttribute(), 1);
+            filters.Add(new HandleErrorAttribute(), 2);
+        }
+    }
+}
---
+++ ELMAHErrorLoggin/Models/ElmahHandleErrorAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Elmah;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ELMAHErrorLoggin.Models
+{
+    /// <summary>
+    /// Create a class inherited by System.Web.Mvc.HandleErrorAttribute.
+    /// </summary>
+    public class ElmahHandleErrorAttribute : HandleErrorAttribute
+    {
+        /// <summary>
+        /// Overriding OnException method to log error occurring in MVC application
+        /// </summary>
+        /// <param name="context"></param>
+        public override void OnException(ExceptionContext context)
+        {
+            base.OnException(context);
+            var e = context.Exception;
+            var httpcontext = HttpContext.Current;
+
+            var signal = ErrorSignal.FromContext(httpcontext);
+            if (signal != null)
+            {
+                signal.Raise(e, httpcontext);
+            }
+        }
+    }
+}
